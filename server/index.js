31c5411c360bc46893b40fa1c550bc1a43e37dab
nihalmenon <nihal200424@gmail.com>
2023-03-07 17:31:26 -0500

    uploaded initial files

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send("Express app")
+})
+
+const port = 3000;
+
+app.listen(3000, () => {
+    console.log('Server is up and running on port ' + port);
+})
