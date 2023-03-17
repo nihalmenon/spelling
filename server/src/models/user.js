d88b270018f5b1f70415f38a145abd29329f9b07
nihalmenon <nihal200424@gmail.com>
2023-03-17 13:43:19 -0400

    initial app setup

---
+++ server/src/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid.')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot include the value password.')
+            }
+        }
+    },
+    tokens: {
+        
+    },
+    avatar: {
+
+    },
+    timestamps: {
+
+    }
+})
