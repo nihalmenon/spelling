43d36e99a3a7776dd49ee8e7f9318c0a95de905f
nihalmenon <nihal200424@gmail.com>
2023-08-22 23:53:57 -0400

    debugging cors

--- server/src/app.js
+++ server/src/app.js
@@ -7,8 +7,18 @@ const gameRouter = require('./routers/game')
 
 const app = express()
 
+console.log('frontend url ' + process.env.FRONTEND_URL)
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      console.log('origin matches frontend url', origin)
+      callback(null, true);
+    } else {
+      console.log('not allowed by cors', origin)
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET','POST','DELETE','UPDATE','PATCH','OPTIONS']
 }));
 
